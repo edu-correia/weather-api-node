fa86b6ae19907f34610db9e36643d642805c7753
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-09-25 17:37:09 -0300

    Jest mocks com o axios

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,14 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass Client', () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
     it('should return the normalized forecast from the StormGlass service', async () => {
         const lat = -43.9011;
         const lng = 153.9129;
 
-        axios.get = jest.fn().mockResolvedValue({data: stormGlassWeather3HoursFixture});
+        mockedAxios.get.mockResolvedValue({data: stormGlassWeather3HoursFixture});
 
-        const stormGlass = new StormGlass(axios);
+        const stormGlass = new StormGlass(mockedAxios);
         const response = await stormGlass.fetchPoints(lat, lng);
 
         expect(response).toEqual(stormGlassNormalized3HoursFixture);
